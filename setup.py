d996455c97179aef2623c83297caafb931575f35
Erik Clarke <ecl@mail.med.upenn.edu>
2015-10-30 10:46:21 -0400

    update setup.py to match pypi

--- setup.py
+++ setup.py
@@ -3,18 +3,19 @@ from setuptools import setup
 VERSION = open('VERSION').read().strip()
 
 setup(
-    name="melting",
+    name="melt",
     py_modules=['melting'],
     version=VERSION,
     author='Erik Clarke',
     author_email='ecl@mail.med.upenn.edu',
     url='https://github.com/eclarke/melt',
     description='A nucleotide melt temp calculator',
-    long_description=(
-        "A nucleotide sequence melt temp calculator for Python, with "
-        "adjustments for monovalent and divalent ion concentrations."),
+    long_description=open('README.rst').read(),
     classifiers=[
-        "Topic :: Scientific/Engineering :: Bio-Informatics"
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License (GPL)'
     ],
+    license='GPL 3',
     entry_points={'console_scripts': ['Tm = melting:main']}
 )
